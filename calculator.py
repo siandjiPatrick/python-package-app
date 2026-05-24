f47c33086813c894325cd5a77bcd6b750f1dc009
patrick Siandji <siandjipatrick@yahoo.fr>
2026-05-24 14:14:07 +0200

    First commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, num1, num2):
+        return num1 + num2
+        
+    def substract(self, num1, num2):
+        return num1 - num2
+    
+    def multiply(self, num1, num2):
+        return num1 * num2
+    
+    def divide(self, num1, num2):
+        return num1 / num2
